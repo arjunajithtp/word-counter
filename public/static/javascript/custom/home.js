d2793c91cabfb44ff043a05d319605cc883ad388
Arjun Ajith <tparjunajith@gmail.com>
2018-05-03 14:57:01 +0530

    corrected css

--- public/static/javascript/custom/home.js
+++ public/static/javascript/custom/home.js
@@ -4,14 +4,13 @@ myApp.controller('AppCtrl', ['$scope', function($scope) {
     $scope.gridData = {};
 
     $scope.getWordCount = function() {
-        
+
         $.ajax({
             url: '/word-counter/home',
             type: 'POST',
             dataType: 'json',
             data : "&url=" + $scope.url,
             success : function(data) {
-                console.log(data);
                 $scope.$apply(function(){
                     $.extend(true,$scope.gridData,data);
                     $scope.keys = Object.keys($scope.gridData);
