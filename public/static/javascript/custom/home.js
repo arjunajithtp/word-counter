a8b95eec05d3bce46d19876ba2831df235f1a842
Arjun Ajith <tparjunajith@gmail.com>
2018-05-03 12:39:55 +0530

    corrected angular

--- public/static/javascript/custom/home.js
+++ public/static/javascript/custom/home.js
@@ -1,16 +1,20 @@
 myApp.controller('AppCtrl', ['$scope', function($scope) {
 
+    $scope.keys = [];
+    $scope.gridData = {};
+
     $scope.getWordCount = function() {
+        
         $.ajax({
             url: '/word-counter/home',
             type: 'POST',
             dataType: 'json',
             data : "&url=" + $scope.url,
             success : function(data) {
-                console.log(data)
+                console.log(data);
                 $scope.$apply(function(){
-                    $scope.words = data;
-                    console.log($scope.words)
+                    $.extend(true,$scope.gridData,data);
+                    $scope.keys = Object.keys($scope.gridData);
                 });
             }
         });
